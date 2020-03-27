e8ed300fd07e13c9c7758e4895f7288fbad5d3e7
AleksaDjordjevic97 <aleksa.b.djordjevic@elfak.rs>
2020-03-27 21:43:57 +0100

    Napravljena ikonica i zapocet rad na RegisterActivity

--- Teammate/app/src/main/java/com/aleksadjordjevic/teammate/RegisterActivity.java
+++ Teammate/app/src/main/java/com/aleksadjordjevic/teammate/RegisterActivity.java
@@ -7,10 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity
 {
+    EditText txtEmail,txtPassword, txtPassword2;
     Button btnRegister;
+    FirebaseAuth mAuth;
+    FirebaseUser user;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,16 +27,12 @@ public class RegisterActivity extends AppCompatActivity
         setContentView(R.layout.activity_register);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        txtEmail = findViewById(R.id.txtEmailR);
+        txtPassword = findViewById(R.id.txtPasswordR);
+        txtPassword2 = findViewById(R.id.txtConfirmPasswordR);
         btnRegister = findViewById(R.id.btnRegisterR);
+        mAuth = FirebaseAuth.getInstance();
 
-        btnRegister.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent reg = new Intent(getApplicationContext(),Register2Activity.class);
-                startActivity(reg);
-            }
-        });
+
     }
 }
