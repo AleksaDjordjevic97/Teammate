e2af82e7f0f6fd080d98275b3f3108db5957091f
AleksaDjordjevic97 <aleksa.b.djordjevic@elfak.rs>
2020-03-28 02:13:23 +0100

    Napravljena registracija, za sada pamti korisnicko ime i profilnu sliku

--- Teammate/app/src/main/java/com/aleksadjordjevic/teammate/Register2Activity.java
+++ Teammate/app/src/main/java/com/aleksadjordjevic/teammate/Register2Activity.java
@@ -1,12 +1,55 @@
 package com.aleksadjordjevic.teammate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Register2Activity extends AppCompatActivity
 {
+    final static int GALLERY_PICK = 1;
+    Button btnRegister;
+    EditText txtUsername;
+    CircleImageView imgProfile;
+    ProgressDialog mDialog;
+    FirebaseAuth mAuth;
+    FirebaseUser user;
+    String userID;
+    StorageReference mStorage;
+    FirebaseFirestore mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -14,5 +57,192 @@ public class Register2Activity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        btnRegister = findViewById(R.id.btnRegisterR2);
+        txtUsername = findViewById(R.id.txtUsernameR2);
+        imgProfile = findViewById(R.id.imgProfileR2);
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        userID = user.getUid();
+        mStorage = FirebaseStorage.getInstance().getReference().child("profile_images");
+        mDatabase = FirebaseFirestore.getInstance();
+
+        btnRegister.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if(mAuth.getCurrentUser() != null)
+                {
+                    saveUserDetails();
+                }
+                else
+                    Toast.makeText(Register2Activity.this,"There was an error. Try again later.",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        imgProfile.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .setAspectRatio(1,1)
+                        .setCropShape(CropImageView.CropShape.OVAL)
+                        .start(Register2Activity.this);
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data)
+    {
+        super.onActivityResult(requestCode,resultCode,data);
+
+
+        if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE)
+        {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+            if(resultCode == RESULT_OK)
+            {
+
+                mDialog = new ProgressDialog(Register2Activity.this);
+                mDialog.setTitle("Profile image");
+                mDialog.setMessage("Please wait while the image is uploaded...");
+                mDialog.show();
+
+                Uri resultUri = result.getUri();
+                final StorageReference filePath = mStorage.child(userID+".jpeg");
+
+                filePath.putFile(resultUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>()
+                {
+
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
+                    {
+                        mDialog.dismiss();
+
+                        filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>()
+                        {
+                            @Override
+                            public void onSuccess(Uri uri)
+                            {
+                                final String uriLink = uri.toString();
+
+                                Map<String,Object> userMap = new HashMap<>();
+                                userMap.put("profile_image",uriLink);
+                                mDatabase.collection("users").document(userID).set(userMap,SetOptions.merge()).addOnCompleteListener(new OnCompleteListener<Void>()
+                                {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task)
+                                    {
+                                        Glide.with(Register2Activity.this)
+                                                .load(uriLink)
+                                                .placeholder(R.drawable.ic_launcher_background)
+                                                .into(imgProfile);
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+
+            }
+            else Toast.makeText(Register2Activity.this,"Doslo je do greske",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void saveUserDetails()
+    {
+        mDialog = new ProgressDialog(Register2Activity.this);
+        mDialog.setMessage("Please wait...");
+        mDialog.show();
+
+        final String username = txtUsername.getText().toString().trim();
+
+        if (username.isEmpty())
+        {
+            mDialog.dismiss();
+            txtUsername.setError("Please enter your username.");
+            txtUsername.requestFocus();
+            return;
+        }
+
+        UserProfileChangeRequest profile = new UserProfileChangeRequest.Builder()
+                .setDisplayName(username)
+                .build();
+
+        user.updateProfile(profile).addOnCompleteListener(new OnCompleteListener<Void>()
+        {
+            @Override
+            public void onComplete(@NonNull Task<Void> task)
+            {
+                if(task.isSuccessful())
+                {
+                    mDialog.dismiss();
+                    Map<String,Object> userMap = new HashMap<>();
+                    userMap.put("username",username);
+                    mDatabase.collection("users").document(userID).set(userMap, SetOptions.merge()).addOnSuccessListener(new OnSuccessListener<Void>()
+                    {
+                        @Override
+                        public void onSuccess(Void aVoid)
+                        {
+                            Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_SHORT).show();
+                            user.sendEmailVerification();
+                            sendToMap();
+                        }
+                    });
+                }
+                else
+                {
+                    mDialog.dismiss();
+                    Toast.makeText(getApplicationContext(), "There was an Error. Try again later.", Toast.LENGTH_SHORT).show();
+                    sendToMain();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Register2Activity.this);
+        builder.setTitle("Exit");
+        builder.setMessage("Please enter your username.");
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    public void sendToMain()
+    {
+        Intent mainIntent = new Intent(getApplicationContext(), Main2Activity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+
+    public void sendToMap()
+    {
+        Intent mapIntent = new Intent(getApplicationContext(), MapActivity.class);
+        mapIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mapIntent);
+        finish();
+    }
+
+
 }
--- Teammate/app/src/main/java/com/aleksadjordjevic/teammate/RegisterActivity.java
+++ Teammate/app/src/main/java/com/aleksadjordjevic/teammate/RegisterActivity.java
@@ -1,21 +1,31 @@
 package com.aleksadjordjevic.teammate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity
 {
     EditText txtEmail,txtPassword, txtPassword2;
     Button btnRegister;
+    ProgressDialog mDialog;
     FirebaseAuth mAuth;
     FirebaseUser user;
     String userID;
@@ -33,6 +43,120 @@ public class RegisterActivity extends AppCompatActivity
         btnRegister = findViewById(R.id.btnRegisterR);
         mAuth = FirebaseAuth.getInstance();
 
+        btnRegister.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                registerUser();
+            }
+        });
+    }
 
+    public void registerUser()
+    {
+        mDialog = new ProgressDialog(RegisterActivity.this);
+        mDialog.setMessage("Please wait...");
+        mDialog.show();
+
+        final String email = txtEmail.getText().toString().trim();
+        final String password = txtPassword.getText().toString().trim();
+        final String password2 = txtPassword2.getText().toString().trim();
+
+        if(checkInputError(email,password,password2))
+        {
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+            {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task)
+                {
+                    mDialog.dismiss();
+
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(getApplicationContext(),"Registration successful!",Toast.LENGTH_SHORT).show();
+
+                        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                        {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task)
+                            {
+                                if(task.isSuccessful())
+                                {
+                                    sendToRegister2();
+                                }
+                                else
+                                    Toast.makeText(getApplicationContext(), "There was an error logging in.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                    else
+                    {
+                        if (task.getException() instanceof FirebaseAuthUserCollisionException)
+                            Toast.makeText(getApplicationContext(), "User with this Email already exists.", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(getApplicationContext(), "There was an error. Try again later.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+        else
+            mDialog.dismiss();
+
+    }
+
+    public boolean checkInputError(String email,String password,String password2)
+    {
+        if(email.isEmpty())
+        {
+            txtEmail.setError("Please enter your Email.");
+            txtEmail.requestFocus();
+            return false;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            txtEmail.setError("Please enter a valid Email address.");
+            txtEmail.requestFocus();
+            return false;
+        }
+
+        if (password.isEmpty())
+        {
+            txtPassword.setError("Please enter your password.");
+            txtPassword.requestFocus();
+            return false;
+        }
+
+        if(password.length() < 6)
+        {
+            txtPassword.setError("Password must contain at least 6 characters.");
+            txtPassword.requestFocus();
+            return false;
+        }
+
+        if (password2.isEmpty())
+        {
+            txtPassword2.setError("Please enter your password.");
+            txtPassword2.requestFocus();
+            return false;
+        }
+
+        if(!password.equals(password2))
+        {
+            txtPassword2.setError("Passwords must match");
+            txtPassword2.requestFocus();
+            return false;
+        }
+
+        return true;
+    }
+
+    public void sendToRegister2()
+    {
+        Intent r2Intent = new Intent(getApplicationContext(), Register2Activity.class);
+        r2Intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(r2Intent);
+        finish();
     }
 }
